e6baa47eeb9444c5b59ee8b4eaa58177f0024dbf
lazyCat <huiliyi.kuangsan@gmail.com>
2022-01-03 20:13:46 +0800

    require modify

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  module.exports = require('./dist/cattus.js')
-} else {
-  module.exports = require('./dist/cattus.min.js')
-}
+module.exports = require('./components')
