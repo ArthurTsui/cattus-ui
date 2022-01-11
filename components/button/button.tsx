f90afcea5fc85239e6208a6f6f5f703f2c08c7ac
lazyCat <huiliyi.kuangsan@gmail.com>
2022-01-11 23:57:16 +0800

    change Button

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,32 +1,33 @@
-import React, { ButtonHTMLAttributes, AnchorHTMLAttributes, ReactNode, CSSProperties, FC } from 'react'
 import classNames from 'classnames'
+import React, { ButtonHTMLAttributes, AnchorHTMLAttributes, ReactNode, CSSProperties, FC } from 'react'
 
-export type ButtonType = 'primary' | 'dashed' | 'link' | 'text' | 'default'
-export type ButtonShape = 'default' | 'circle' | 'round'
+export type ButtonType = 'primary' | 'danger' | 'success' | 'warning' | 'info' | 'default'
+export type ButtonFacade = 'contained' | 'outlined' | 'text'
 export type ButtonSize = 'large' | 'middle' | 'small'
+export type ButtonShape = 'rect' | 'roundRect' | 'circle' | 'radian'
 export type HtmlType = 'submit' | 'reset' | 'button' | undefined
 
 //基础props类型定义
 interface BaseButtonProps {
   type?: ButtonType
-  danger?: boolean
-  ghost?: boolean
-  shape?: ButtonShape
+  facade?: ButtonFacade
   size?: ButtonSize
+  shape?: ButtonShape
   htmlType?: HtmlType
-  icon?: ReactNode | string
   children?: ReactNode
-  className?: string
+  endIcon?: ReactNode | string
+  startIcon?: ReactNode | string
   style?: CSSProperties
+  className?: string
 }
 
-type NativeButtonProps = BaseButtonProps & Omit<ButtonHTMLAttributes<HTMLElement>, 'type'> // 按钮类型，props类型定义
 type AnchorButtonProps = BaseButtonProps & Omit<AnchorHTMLAttributes<HTMLElement>, 'type'> // a标签类型，props类型定义
+type NativeButtonProps = BaseButtonProps & Omit<ButtonHTMLAttributes<HTMLElement>, 'type'> // 按钮类型，props类型定义
 
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps> // button最终的props类型定义
 
 const Button: FC<ButtonProps> = (props: ButtonProps) => {
-  const { className, type, danger, ghost, shape, size, icon, children, href, htmlType, style, ...rest } = props
+  const { type, facade, size, shape, htmlType, startIcon, endIcon, children, href, className, style, ...rest } = props
 
   const sizeClassNameMap = { large: 'lg', middle: 'md', small: 'sm' }
   const sizeCls = size ? sizeClassNameMap[size] : undefined
@@ -35,10 +36,9 @@ const Button: FC<ButtonProps> = (props: ButtonProps) => {
     'cat-btn',
     {
       [`cat-btn-${type}`]: type,
+      [`cat-btn-${facade}`]: facade,
       [`cat-btn-${sizeCls}`]: sizeCls,
-      [`cat-btn-shape-${shape}`]: shape,
-      [`cat-btn-danger`]: danger,
-      [`cat-btn-ghost`]: ghost
+      [`cat-btn-${shape}`]: shape
     },
     className
   )
@@ -46,29 +46,29 @@ const Button: FC<ButtonProps> = (props: ButtonProps) => {
   if (href) {
     return (
       <a href={href} className={classes} style={style} {...rest}>
-        {typeof icon !== 'string' ? icon : <img src={icon} alt='' className='cat-btn-icon-img' />}
+        {typeof startIcon !== 'string' ? startIcon : <img src={startIcon} alt='' className='cat-btn-icon-img' />}
         {children}
+        {typeof endIcon === 'string' ? <img src={endIcon} alt='' className='cat-btn-icon-img' /> : endIcon}
       </a>
     )
   }
 
   return (
     <button className={classes} type={htmlType} style={style} {...rest}>
-      {typeof icon !== 'string' ? icon : <img src={icon} alt='' className='cat-btn-icon-img' />}
+      {typeof startIcon === 'string' ? <img src={startIcon} alt='' className='cat-btn-icon-img' /> : startIcon}
       {children}
+      {typeof endIcon === 'string' ? <img src={endIcon} alt='' className='cat-btn-icon-img' /> : endIcon}
     </button>
   )
 }
 
 //默认props
 Button.defaultProps = {
-  type: 'default',
+  type: 'primary',
+  facade: 'contained',
   size: 'middle',
-  disabled: false,
-  danger: false,
-  ghost: false,
-  htmlType: 'button',
-  shape: 'default'
+  shape: 'roundRect',
+  htmlType: 'button'
 }
 
 export default Button
